a02ca0c700c2ab60a327b35b10b1fedb0d4150b3
yashmalviya27 <138759744+yashmalviya27@users.noreply.github.com>
2024-05-05 11:10:10 +0000

    convation_confusion_3

---
+++ yash/convation_confusion_3.js
@@ -0,0 +1,10 @@
+let score = "22"
+console.log(score)
+console.log(typeof score)
+
+let valueInNumber = Number(score)
+console.log(typeof valueInNumber)
+
+ 
+// 33 is easily convert in Number
+// but "33acbb" is no convert and show NaN "not a number"

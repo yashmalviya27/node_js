07fe38dd7070a2885e1a9405db08628b0fbab123
yashmalviya27 <138759744+yashmalviya27@users.noreply.github.com>
2024-05-05 11:28:15 +0000

    convation_confusion_3

--- yash/convation_confusion_3.js
+++ yash/convation_confusion_3.js
@@ -8,3 +8,7 @@ console.log(typeof valueInNumber)
  
 // 33 is easily convert in Number
 // but "33acbb" is no convert and show NaN "not a number"
+let str1 = "yash "
+let str2 = "malviya"
+let str3 = str1+str2
+console.log(str3)
